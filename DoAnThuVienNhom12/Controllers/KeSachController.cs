9cb7515ea87a3089d04aa393a62869b673fdb89e
Chen Dokutah <thinhdepzai789@gmail.com>
2025-11-03 21:55:04 +0700

    a

---
+++ DoAnThuVienNhom12/Controllers/KeSachController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DoAnThuVienNhom12.Controllers
+{
+    public class KeSachController : Controller
+    {
+        // GET: KeSach
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Quản lý kệ sách";
+            return View();
+        }
+
+        // GET: KeSach/Detail/KE_A
+        public ActionResult Detail(string id)
+        {
+            ViewBag.Title = "Chi tiết kệ sách";
+            ViewBag.ShelfCode = id;
+            return View();
+        }
+    }
+}
